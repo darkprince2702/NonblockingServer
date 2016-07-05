9c81b84ce9454b9e9c37a46367da40ec80751f11
ductn <ductn@ductn-HP-Compaq-Pro-6300-MT>
2016-07-05 18:13:07 +0700

    Server ok

--- Client.cpp
+++ Client.cpp
@@ -6,7 +6,6 @@
 
 #include "NonblockingServer.h"
 
-
 Client::Client(int port) : protocol_(new Protocol()) {
     port_ = port;
     connect();
@@ -22,41 +21,53 @@ void Client::connect() {
     int clientSocket, status;
     struct addrinfo hints, *res, *res0;
     struct sockaddr_storage serverAddr;
-    
-    memset(&hints, 0, sizeof(hints));
+
+    memset(&hints, 0, sizeof (hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
     if ((status = getaddrinfo(NULL, port_, &hints, &res)) != 0) {
-		std::cout << "Get host address error" << gai_strerror(status) << std::endl;
-		return 1;
-	}
-    for (res0 = res; res0 != NULL; res0 = res0->ai_next) {
-        if ((clientSocket = 
-                socket(res0->ai_family, res0->ai_socktype, res0->ai_protocol)) 
-                == -1) {
-			std::cout << "Get socket FD error" << std::endl;
-			continue;
-		}
-
-		if(connect(clientSocket, res0->ai_addr, res0->ai_addrlen) == -1) {
-			close(clientSocket);
-			perror("Connect error");
-			continue;
-		}
-		break;
+        std::cout << "Get host address error" << gai_strerror(status) << std::endl;
+        return 1;
     }
-    
+    for (res0 = res; res0 != NULL; res0 = res0->ai_next) {
+        if ((clientSocket =
+                socket(res0->ai_family, res0->ai_socktype, res0->ai_protocol))
+                == -1) {
+            std::cout << "Get socket FD error" << std::endl;
+            continue;
+        }
+
+        if (connect(clientSocket, res0->ai_addr, res0->ai_addrlen) == -1) {
+            close(clientSocket);
+            perror("Connect error");
+            continue;
+        }
+        break;
+    }
+
     if (clientSocket) {
         clientSocket_ = clientSocket;
     }
 }
 
-
 void Client::disconnect() {
     close(clientSocket_);
 }
 
+void Client::clientSend(Operator* o) {
+    std::shared_ptr<Message> message(protocol_->processInput(&o));
+    int writePos = 0, writeSize = message->size, left, sent;
+    while (writePos < writeSize) {
+        left = writeSize - writePos;
+        sent = write(clientSocket_, message->content + writePos, left);
+        writePos += sent;
+    }
+}
+
+Operator* Client::clientReceive() {
+    
+}
 
 
 bool Client::set(std::string key, std::string value) {
@@ -65,15 +76,10 @@ bool Client::set(std::string key, std::string value) {
     o.type = "set";
     o.key = key;
     o.value = value;
+    
+    clientSend(&o);
 
-    std::shared_ptr<Message> message(protocol_->processInput(&o));
-    int writePos = 0, writeSize = message->size, left, sent;
-    while (writePos < writeSize) {
-        left = writeSize - writePos;
-        sent = write(clientSocket_, message->content + writePos, left);
-        writePos += sent;
-    }
-    
-    
+
+
 }
 
